74724bc83e7344cfeac64b03cd507fe9cf6473c6
geeth22 <geethchandra22@gmail.com>
2020-11-20 20:38:04 +0530

    Solving using Switch Case Statement

--- empwage.js
+++ empwage.js
@@ -10,9 +10,16 @@ let empHrs = 0;
 let empWage = 0;
 
 let empCheck = Math.floor(Math.random() * 3);
-if (empCheck == IS_PART_TIME) empHrs = 4;
-else if (empCheck == IS_FULL_TIME) empHrs = 8;
-else empHrs = 0;
+switch (empCheck) {
+  case IS_PART_TIME:
+    empHrs = 4;
+    break;
+  case IS_FULL_TIME:
+    empHrs = 8;
+    break;
+  default:
+    empHrs = 0;
+}
 
 empWage = empHrs * EMP_RATE_PER_HOUR;
 console.log("Employee Wage is: " + empWage);
