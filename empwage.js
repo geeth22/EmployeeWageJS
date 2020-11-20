e75baf1338e250700d7d6708115049038d772e81
geeth22 <geethchandra22@gmail.com>
2020-11-20 20:34:52 +0530

    Add part time employee and wage

--- empwage.js
+++ empwage.js
@@ -1,11 +1,17 @@
-//Calculate Daily Employee Wage
+//Add Part time Employee & Wage
 
+//Constants
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 let EMP_RATE_PER_HOUR = 20;
+
+//Variables
 let empHrs = 0;
 let empWage = 0;
-let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 2);
-if (empCheck == 1) empHrs = 8;
+
+let empCheck = Math.floor(Math.random() * 3);
+if (empCheck == IS_PART_TIME) empHrs = 4;
+else if (empCheck == IS_FULL_TIME) empHrs = 8;
 else empHrs = 0;
 
 empWage = empHrs * EMP_RATE_PER_HOUR;
