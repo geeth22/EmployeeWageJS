6ba87a106b206029d264e1957fac1ef193c1185f
geeth22 <geethchandra22@gmail.com>
2020-11-20 20:26:57 +0530

    check whether employee present or absent

--- empwage.js
+++ empwage.js
@@ -1,2 +1,6 @@
-var msg = "Welcome to employee wage computation program";
-console.log(msg);
+//Check Employee is Present or Absent
+
+let isPresent = 1;
+let empCheck = Math.floor(Math.random() * 2);
+if (empCheck == 1) console.log("Employee is Present");
+else console.log("Employee is Absent");
